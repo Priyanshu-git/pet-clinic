3f2dcac33680827446190786660752fd2adcc31e
Priyanshu Gaurav <gauravpriyanshu3@gmail.com>
2021-05-31 09:06:47 +0530

    Updated vet with JPA entity mappings. closes #37

--- pet-clinic-data/src/main/java/spring/framework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/spring/framework/petclinic/model/Speciality.java
@@ -1,6 +1,14 @@
 package spring.framework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
+
+    @Column(name = "description")
     String description;
 
     public String getDescription() {
